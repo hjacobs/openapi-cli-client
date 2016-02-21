1ef68747f4f6494c52d54835a9938507c5effada
hjacobs <henning@jacobs1.de>
2016-02-21 14:25:04 +0100

    fire first requests

--- openapi_cli_client/cli.py
+++ openapi_cli_client/cli.py
@@ -1,10 +1,14 @@
 import click
 import clickclick
 import re
+import requests
 import sys
 import yaml
 
+from functools import partial
 from bravado_core.spec import Spec
+from bravado.client import construct_request
+from bravado.requests_client import RequestsClient
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
@@ -31,23 +35,47 @@ def get_command_name(op):
         return op.http_method
 
 
+def invoke(op, *args, **kwargs):
+    request = construct_request(op, {}, **kwargs)
+    c = RequestsClient()
+    future = c.request(request)
+    future.result()
+
+
+def sanitize_spec(spec):
+
+    for path, path_obj in list(spec['paths'].items()):
+        # remove root paths as no resource name can be found for it
+        if path == '/':
+            del spec['paths'][path]
+    return spec
+
+
 def generate_cli(spec):
+    origin_url = None
     if isinstance(spec, str):
-        with open(spec, 'rb') as fd:
-            spec = yaml.safe_load(fd.read())
+        if spec.startswith('https://') or spec.startswith('http://'):
+            origin_url = spec
+            r = requests.get(spec)
+            r.raise_for_status()
+            spec = yaml.safe_load(r.text)
+        else:
+            with open(spec, 'rb') as fd:
+                spec = yaml.safe_load(fd.read())
+
+    spec = sanitize_spec(spec)
 
     cli = clickclick.AliasedGroup(context_settings=CONTEXT_SETTINGS)
 
-    spec = Spec.from_dict(spec)
+    spec = Spec.from_dict(spec, origin_url=origin_url)
     for res_name, res in spec.resources.items():
         grp = clickclick.AliasedGroup(normalize_command_name(res_name), short_help='Manage {}'.format(res_name))
         cli.add_command(grp)
         for op_name, op in res.operations.items():
-            print(op.__dict__)
             name = get_command_name(op)
-            cmd = click.Command(name, short_help=op.op_spec.get('summary'))
+
+            cmd = click.Command(name, callback=partial(invoke, op=op), short_help=op.op_spec.get('summary'))
             for param_name, param in op.params.items():
-                print(param.__dict__)
                 if param.required:
                     arg = click.Argument([param.name])
                     cmd.params.append(arg)
@@ -55,10 +83,7 @@ def generate_cli(spec):
                     arg = click.Option(['--' + param.name])
                     cmd.params.append(arg)
 
-                print(param)
             grp.add_command(cmd)
-        print(cli.__dict__)
-        #setattr(cli, res_name.lower(), cli.group())
 
     return cli
 
